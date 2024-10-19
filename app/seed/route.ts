b9da6718f6e1f4b650f4fc85c19f4d6efd739aa4
puntoenv <sebssjb@gmail.com>
2024-10-19 15:26:34 -0500

    ADD: graphics

--- app/seed/route.ts
+++ app/seed/route.ts
@@ -1,6 +1,6 @@
-// import bcrypt from 'bcrypt';
-// import { db } from '@vercel/postgres';
-// import { invoices, customers, revenue, users } from '../lib/placeholder-data';
+// import bcrypt from "bcrypt";
+// import { db } from "@vercel/postgres";
+// import { invoices, customers, revenue, users } from "../lib/placeholder-data";
 
 // const client = await db.connect();
 
@@ -101,22 +101,22 @@
 //   return insertedRevenue;
 // }
 
-export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
-  // try {
-  //   await client.sql`BEGIN`;
-  //   await seedUsers();
-  //   await seedCustomers();
-  //   await seedInvoices();
-  //   await seedRevenue();
-  //   await client.sql`COMMIT`;
+// export async function GET() {
+//   // return Response.json({
+//   //   message:
+//   //     'Uncomment this file and remove this line. You can delete this file when you are finished.',
+//   // });
+//   try {
+//     await client.sql`BEGIN`;
+//     await seedUsers();
+//     await seedCustomers();
+//     await seedInvoices();
+//     await seedRevenue();
+//     await client.sql`COMMIT`;
 
-  //   return Response.json({ message: 'Database seeded successfully' });
-  // } catch (error) {
-  //   await client.sql`ROLLBACK`;
-  //   return Response.json({ error }, { status: 500 });
-  // }
-}
+//     return Response.json({ message: "Database seeded successfully" });
+//   } catch (error) {
+//     await client.sql`ROLLBACK`;
+//     return Response.json({ error }, { status: 500 });
+//   }
+// }
